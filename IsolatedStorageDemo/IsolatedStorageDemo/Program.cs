eca588a4621b3f52f5539f0e22163cad14479aaf
BiroMarton <rezoner9@gmail.com>
2017-06-18 12:50:55 +0200

    Isolated storage exercise implemented.

---
+++ IsolatedStorageDemo/IsolatedStorageDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.IsolatedStorage;
+using System.Threading.Tasks;
+
+namespace IsolatedStorageDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsolatedStorageFile userStore = IsolatedStorageFile.GetUserStoreForAssembly();
+
+            IsolatedStorageFileStream userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Create, userStore);
+
+            StreamWriter userWriter = new StreamWriter(userStream);
+            userWriter.WriteLine("Kod Adi K.O.Z. A look at the 17-25 December 2013 corruption scandal in Turkey, from the viewpoint of the Erdogan government.");
+            userWriter.Close();
+        }
+    }
+}
