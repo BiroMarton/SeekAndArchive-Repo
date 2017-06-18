e8ed9b2892bc8c09775a7fbebad4c46d31253182
BiroMarton <rezoner9@gmail.com>
2017-06-18 12:03:49 +0200

    Implemented Searching for a file exercise.

---
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeekAndArchive
+{
+    class Program
+    {
+        static List<FileInfo> FoundFiles;
+
+        static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
+        {
+            foreach (FileInfo fil in currentDirectory.GetFiles())
+            {
+                if (fil.Name == fileName)
+                    foundFiles.Add(fil);
+            }
+            foreach (DirectoryInfo dir in currentDirectory.GetDirectories())
+            {
+                RecursiveSearch(foundFiles, fileName, dir);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Application started.");
+            string fileName = args[0];
+            string directoryName = args[1];
+            FoundFiles = new List<FileInfo>();
+
+            DirectoryInfo rootDir = new DirectoryInfo(directoryName);
+            if (!rootDir.Exists)
+            {
+                Console.WriteLine("The specified directory does not exist.");
+                return;
+            }
+
+            RecursiveSearch(FoundFiles, fileName, rootDir);
+            Console.WriteLine("Found {0} files.", FoundFiles.Count);
+            foreach (FileInfo fil in FoundFiles)
+            {
+                Console.WriteLine("{0}", fil.FullName);
+            }
+            Console.ReadKey();
+        }
+    }
+}
