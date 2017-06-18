4c1dfd0aaca73314fb18125ff8f95ed041597698
BiroMarton <rezoner9@gmail.com>
2017-06-18 12:18:32 +0200

    File watching exercise done.

--- SeekAndArchive/SeekAndArchive/Program.cs
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -10,6 +10,7 @@ namespace SeekAndArchive
     class Program
     {
         static List<FileInfo> FoundFiles;
+        static List<FileSystemWatcher> watchers;
 
         static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
         {
@@ -24,12 +25,19 @@ namespace SeekAndArchive
             }
         }
 
+        static void WatcherChanged(object sender, FileSystemEventArgs e)
+        {
+            if (e.ChangeType == WatcherChangeTypes.Changed)
+                Console.WriteLine("{0} has been changed!", e.FullPath);
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Application started.");
             string fileName = args[0];
             string directoryName = args[1];
             FoundFiles = new List<FileInfo>();
+            watchers = new List<FileSystemWatcher>();
 
             DirectoryInfo rootDir = new DirectoryInfo(directoryName);
             if (!rootDir.Exists)
@@ -43,6 +51,10 @@ namespace SeekAndArchive
             foreach (FileInfo fil in FoundFiles)
             {
                 Console.WriteLine("{0}", fil.FullName);
+                FileSystemWatcher newWatcher = new FileSystemWatcher(fil.DirectoryName, fil.Name);
+                newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
+                newWatcher.EnableRaisingEvents = true;
+                watchers.Add(newWatcher);
             }
             Console.ReadKey();
         }
