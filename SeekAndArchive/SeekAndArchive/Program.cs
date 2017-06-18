8edba4c373b59a76d4cc5f38d5baac525b885b93
BiroMarton <rezoner9@gmail.com>
2017-06-18 12:42:58 +0200

    Archive creating exercise done.

--- SeekAndArchive/SeekAndArchive/Program.cs
+++ SeekAndArchive/SeekAndArchive/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace SeekAndArchive
     {
         static List<FileInfo> FoundFiles;
         static List<FileSystemWatcher> watchers;
+        static List<DirectoryInfo> archiveDirs;
 
         static void RecursiveSearch(List<FileInfo> foundFiles, string fileName, DirectoryInfo currentDirectory)
         {
@@ -29,6 +31,27 @@ namespace SeekAndArchive
         {
             if (e.ChangeType == WatcherChangeTypes.Changed)
                 Console.WriteLine("{0} has been changed!", e.FullPath);
+            System.Threading.Thread.Sleep(500);
+            FileSystemWatcher senderWatcher = (FileSystemWatcher)sender;
+            int index = watchers.IndexOf(senderWatcher, 0);
+            ArchiveFile(archiveDirs[index], FoundFiles[index]);
+        }
+
+        static void ArchiveFile(DirectoryInfo archiveDir, FileInfo fileToArchive)
+        {
+            FileStream input = fileToArchive.OpenRead();
+            FileStream output = File.Create(archiveDir.FullName + @"\" + fileToArchive.Name + ".gz");
+            GZipStream Compressor = new GZipStream(output, CompressionMode.Compress);
+            int b = input.ReadByte();
+            while (b != -1)
+            {
+                Compressor.WriteByte((byte)b);
+
+                b = input.ReadByte();
+            }
+            Compressor.Close();
+            input.Close();
+            output.Close();
         }
 
         static void Main(string[] args)
@@ -55,6 +78,12 @@ namespace SeekAndArchive
                 newWatcher.Changed += new FileSystemEventHandler(WatcherChanged);
                 newWatcher.EnableRaisingEvents = true;
                 watchers.Add(newWatcher);
+            }
+
+            archiveDirs = new List<DirectoryInfo>();
+            for (int i = 0; i < FoundFiles.Count; i++)
+            {
+                archiveDirs.Add(Directory.CreateDirectory(directoryName + "\\archive" + i.ToString()));
             }
             Console.ReadKey();
         }
