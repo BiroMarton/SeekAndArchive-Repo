8f290447318407443b22ebdb3bcb4957ad046e9e
BiroMarton <rezoner9@gmail.com>
2017-06-18 12:59:02 +0200

    Implemented Reading a File in Isolated Storage exercise.

--- IsolatedStorageDemo/IsolatedStorageDemo/Program.cs
+++ IsolatedStorageDemo/IsolatedStorageDemo/Program.cs
@@ -19,6 +19,23 @@ namespace IsolatedStorageDemo
             StreamWriter userWriter = new StreamWriter(userStream);
             userWriter.WriteLine("Kod Adi K.O.Z. A look at the 17-25 December 2013 corruption scandal in Turkey, from the viewpoint of the Erdogan government.");
             userWriter.Close();
+
+            string[] files = userStore.GetFileNames("UserSettings.set");
+            if (files.Length == 0)
+            {
+                Console.WriteLine("No files found!");
+                Console.WriteLine("Press Enter to exit.");
+                Console.ReadKey();
+
+            }
+
+            userStream = new IsolatedStorageFileStream("UserSettings.set", FileMode.Open, userStore);
+            StreamReader userReader = new StreamReader(userStream);
+            string contents = userReader.ReadToEnd();
+
+            Console.WriteLine(contents);
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadKey();
         }
     }
 }
